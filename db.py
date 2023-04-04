b97090f49708e92e1536d7e2c3580c4ac3dfe715
Sergey <seryoga-lebedev@yandex.ru>
2023-04-04 23:08:32 +0300

    add file

---
+++ db.py
@@ -0,0 +1,43 @@
+import psycopg2
+from psycopg2 import OperationalError
+
+
+class DB:
+    def __init__(self, database='ems_prod', user='postgres', password='seryoga1989'):
+        self.connect = psycopg2.connect(database=database, user=user, password=password, host="127.0.0.1", port="5432")
+        with self.connect.cursor() as cur:
+            try:
+                cur.execute("""
+                    CREATE TABLE IF NOT EXISTS folders (
+                        id SERIAL PRIMARY KEY,
+                        name VARCHAR NOT NULL,
+                        surname VARCHAR NOT NULL
+                    );
+                """)
+                self.connect.commit()
+                print('база данных успешно создана')
+            except OperationalError as e:
+                print(f'Произошла ошибка {e}')
+
+    def insert_client(self, name: str, surname: str):
+        with self.connect.cursor() as cur:
+            try:
+                cur.execute("""
+                    INSERT INTO folders(
+                        name,
+                        surname)
+                    VALUES(
+                        %s, %s
+                    );
+            """, (name, surname))
+                self.connect.commit()
+                print(f'клиент {name} {surname} успешно добавлен')
+            except OperationalError as e:
+                print(f'Произошла ошибка при добавлении клиента {e}')
+
+    def close_connect(self):
+        self.close_connect()
+
+
+add = DB().insert_client('Петр', 'Петров')
+add1 = DB().insert_client('Иван', 'Иванов')
