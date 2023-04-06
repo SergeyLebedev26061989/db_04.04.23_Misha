c898a6ba1b4704ff05acfd06b8fc7d7d9327673a
SergeyLebedev26061989 <109577543+SergeyLebedev26061989@users.noreply.github.com>
2023-04-06 17:11:28 +0300

    add look id

--- db.py
+++ db.py
@@ -58,8 +58,7 @@ add = DB().insert_client('Петр', 'Петров')
 add1 = DB().insert_client('Иван', 'Иванов')
 
 look_db = DB().look_db()
-id_people = []
-for i in look_db:
-    id_people.append(i[0])
+for id in look_db:
+    id_look = DB().read_db(f'https://www.google.com/{id[0]}')
     time.sleep(0.2)
-print(id_people)
+    print(id_look)
